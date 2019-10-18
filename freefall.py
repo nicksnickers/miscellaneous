29fd64c1571c0833a0e186fc177d8e1d4069e154
Nicholas Haddad <nthaddad60@gmail.com>
2019-10-18 16:27:39 -0500

    Yeetus

--- freefall.py
+++ freefall.py
@@ -1,13 +1,15 @@
 
 # Program for calculating the free fall time w/o air resistance at 9.8 m/s given the distance.
 
+import math
+
 def falltime(d):
     x = float(d)/4.9
-    t = pow(x, (1/2))
+    t = math.sqrt(x)
     return t
 
 print("This program calculates the time for free fall based on the distance given.")
-distance = input("Insert the distance for the fall:")
+distance = input("Insert the distance for the fall: ")
 
 time = falltime(distance)
 print("The fall will take " + str(time) + " seconds.")
